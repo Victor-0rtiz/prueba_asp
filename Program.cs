439b8268094c828816d0a0b3f55c8be9f44bcb51
Victor Oss <vicjosneros@gmail.com>
2023-09-01 22:02:53 -0600

    Prueba con asp

--- Program.cs
+++ Program.cs
@@ -24,4 +24,11 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+
+app.MapControllerRoute(
+name: "prueba",
+pattern: "/prueba",
+defaults: new { controller = "Home", action = "Prueba" });
+
+
 app.Run();
